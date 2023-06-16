a3bff254ce0c8915a6dd10be98ff8a0b1db04bad
Kitwana Akil <kakil@me.com>
2023-06-16 10:17:09 -0400

    Initial file creation.

---
+++ SwiftUI_Views/LayoutViews/VerticalStack/KVStack_Nesting.swift
@@ -0,0 +1,21 @@
+//
+//  KVStack_Nesting.swift
+//  SwiftUI_Views
+//
+//  Created by Kitwana Akil on 6/16/23.
+//  Copyright © 2023 Mark Moeykens. All rights reserved.
+//
+
+import SwiftUI
+
+struct KVStack_Nesting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KVStack_Nesting_Previews: PreviewProvider {
+    static var previews: some View {
+        KVStack_Nesting()
+    }
+}
